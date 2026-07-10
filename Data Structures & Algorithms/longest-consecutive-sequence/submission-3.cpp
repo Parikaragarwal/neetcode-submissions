d22d35c7913c054da86f580fd09abccdddb0a076
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-07-10 12:19:42 +0530

    Add: longest-consecutive-sequence - submission-3

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-3.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    //  bool checkExists(vector<int> &nums,int target){
+    //   for(int el:nums){
+    //     if(el==target){
+    //       return true;
+    //     }
+    //   }
+    //   return false;
+    // }
+
+    int findChain(unordered_map<int,int> &chain,
+    unordered_set<int> &s
+    ,int n){
+      if(chain.contains(n)){
+        return chain[n];
+      }
+      if(!s.contains(n-1)){
+        chain[n]=1;
+        return 1;
+      }
+      chain[n] = findChain(chain,s,n-1)+1;
+      return chain[n];
+    }
+
+    int longestConsecutive(vector<int>& nums) {
+
+        // unordered_set<int> elements(nums.begin(),nums.end());
+        // int ans = 0;
+        // for(int el:nums){
+        //   int chainln = 1;
+        //   int chainel = el;
+        //   while(elements.contains(chainel+1)){
+        //     chainln++;
+        //     chainel++;
+        //   }
+        //   ans = max(ans,chainln);
+        // }
+        // return ans;
+
+        // set<int> sorter(nums.begin(),nums.end());
+        // vector<int> arr(sorter.begin(),sorter.end());
+        // int ans=0;
+        // for(int i=0;i<arr.size();i++){
+        //   int chainel = arr[i];
+        //   int chainln = 1;
+        //   while(i<arr.size()-1 && arr[i+1]==arr[i]+1 ){
+        //     chainln++;
+        //     i++;
+        //   }
+        //   ans= max(ans,chainln);
+        // }
+        // return ans;
+
+        unordered_map<int,int> chain;
+        unordered_set<int> s(nums.begin(),nums.end());
+        int ans=0;
+        for(int el:s){         
+          ans= max(ans,findChain(chain,s,el));
+        }
+        return ans;
+    }
+};
