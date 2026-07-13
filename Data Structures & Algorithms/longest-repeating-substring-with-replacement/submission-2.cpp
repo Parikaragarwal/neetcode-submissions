0b54431e46a1d902dbba65fd445f84f25d3ad666
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-07-13 14:49:09 +0530

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public: 
+    int cti(char ch){
+        return ch-'A';
+    }
+    int maxfeq(vector<int> &feq){
+        int mx = INT_MIN;
+        for(int el:feq){
+            mx = max(mx,el);
+        }
+        return mx;
+    }
+    int characterReplacement(string s, int k) {
+        int l=0;
+        int r=0;
+        int size = s.size();
+        int mxf = 0;
+        vector<int> feq(26,0);
+        int ans=0;
+        
+
+        while(r<size){
+            int id = cti(s[r]);
+            feq[id]++;
+            int win = r-l+1;
+            mxf = max(mxf,feq[id]);
+            while(r-l+1-mxf>k){
+                feq[cti(s[l])]--;
+                l++;
+                win= r-l+1;
+            }
+          
+            ans = max(ans,r-l+1);
+            r++;
+        }
+        return ans;
+    }
+};
