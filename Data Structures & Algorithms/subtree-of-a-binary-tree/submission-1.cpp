7951501733d3cadc21cc29b17b4d2478cf0b0bd9
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-07-24 19:02:39 +0530

    Add: subtree-of-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-1.cpp
@@ -0,0 +1,126 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+private:
+ bool isSameTree(TreeNode* p, TreeNode* q) {
+
+        if(!p && !q){
+            return true;
+        }else if((!p && q)|| (!q && p)){
+            return false;
+        }
+
+        // return p->val==q->val && 
+        // isSameTree(p->left,q->left) && 
+        // isSameTree(p->right,q->right);
+
+
+
+        // stack<TreeNode*> ps;
+        // stack<TreeNode*> qs;
+
+        // ps.push(p);
+        // qs.push(q);
+        // while(!ps.empty() && !qs.empty()){
+            
+        //     TreeNode* node1 = ps.top();
+        //     ps.pop();
+
+        //     TreeNode* node2 = qs.top();
+        //     qs.pop();
+
+        //     if((node1 && !node2) || (node2 && !node1)){
+        //         return false;
+        //     }
+
+        //     if((node1 && node2))
+        //     {
+        //         if(node1->val==node2->val){
+        //         ps.push(node1->left);
+        //         ps.push(node1->right);
+        //         qs.push(node2->left);
+        //         qs.push(node2->right);
+        //         }else{
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+
+        queue<TreeNode*> pq;
+        queue<TreeNode*> qq;
+
+        pq.push(p);
+        qq.push(q);
+
+        while(!pq.empty() && !qq.empty()){
+            int pqs = pq.size();
+            int qqs = qq.size();
+            if(pqs!=qqs){
+                return false;
+            }
+
+            for(int i=0;i<pqs;i++){
+                TreeNode* node1= pq.front();
+                pq.pop();
+                TreeNode* node2= qq.front();
+                qq.pop();
+
+                if((node1 && !node2) || (node2 && !node1)){
+                return false;
+                }
+
+            if((node1 && node2))
+            {
+                if(node1->val==node2->val){
+                pq.push(node1->left);
+                pq.push(node1->right);
+                qq.push(node2->left);
+                qq.push(node2->right);
+                }else{
+                    return false;
+                }
+            }
+            }
+        }
+        return true;
+    }
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(!subRoot){
+            return true;
+        }
+
+        if(!root && subRoot){
+            return false;
+        }
+
+        stack<TreeNode*> s;
+        s.push(root);
+
+        while(!s.empty()){
+            TreeNode* node = s.top();
+            s.pop();
+            if(node->val==subRoot->val && isSameTree(node,subRoot)){
+                return true;
+            }
+            if(node->left){
+                s.push(node->left);
+            }
+            if(node->right){
+                s.push(node->right);
+            }
+        }
+        return false;
+    }
+};
