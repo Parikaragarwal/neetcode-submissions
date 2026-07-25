1647505ff617567f959dde71e839466e5757aacf
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-07-25 14:02:16 +0530

    Add: lowest-common-ancestor-in-binary-search-tree - submission-10

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-10.cpp
@@ -0,0 +1,71 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool checkOneExists(TreeNode* root,TreeNode* p){
+        if(!root){
+            return false;
+        }
+        if(root==p){
+            return true;
+        }
+        return checkOneExists(root->left,p) || checkOneExists(root->right,p);
+    }
+    bool checkExists(TreeNode* root, TreeNode* p, TreeNode* q,TreeNode*& ans){
+        if(!root){
+            return false;
+        }
+        if(ans){
+            return true;
+        }
+        
+        if(
+            !(checkExists(root->left,p,q,ans)
+            ||
+            checkExists(root->right,p,q,ans))
+            &&
+            checkOneExists(root,p)
+            && 
+            checkOneExists(root,q)
+            ){
+                ans=root;
+                return true;
+            }else{
+                return false;
+            }
+            
+    }
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        // TreeNode* ans = NULL;
+        // checkExists(root,p,q,ans);
+        // return ans;
+        TreeNode* curr = root;
+        
+        while(true){
+        if(!curr){
+            return curr;
+        }
+        int a=curr->val;
+        int b=p->val;
+        int c=q->val;
+            if(a<b && a<c){
+                curr=curr->right;
+            }else if(a>b && a>c){
+                curr=curr->left;
+            }else{
+                return curr;
+            }
+        }
+        return root;
+    }
+};
