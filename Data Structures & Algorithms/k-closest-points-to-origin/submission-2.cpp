d5a301e7d4c3ac0b42dc2d4950a38c00740a75d0
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-07-27 14:34:40 +0530

    Add: k-closest-points-to-origin - submission-2

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-2.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    static int distance(vector<int> &point){
+        int a=point[0];
+        int b=point[1];
+
+        return (a*a+b*b);
+    }
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+
+        auto cmp = [](vector<int> &a,
+        vector<int> &b){
+            return distance(a)>distance(b);
+        };
+
+        priority_queue<
+        vector<int>,
+        vector<vector<int>>,
+        decltype(cmp)
+        > pq(cmp,points);
+
+        vector<vector<int>> ans;
+        while(k--){
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+};
