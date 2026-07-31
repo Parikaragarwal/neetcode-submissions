9a546adc5db044f46dac0e30d26a2dddaf7828df
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-07-31 16:24:50 +0530

    Add: kth-largest-element-in-an-array - submission-4

---
+++ Data Structures & Algorithms/kth-largest-element-in-an-array/submission-4.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    int partition(vector<int> &nums,int s,int e){
+        int pid = s + rand() % (e - s + 1);
+        int pivot = nums[pid];
+        swap(nums[pid],nums[e]);
+        int i=s;
+        int j=e-1;
+        while(true){
+            while(i<e && nums[i]<pivot){
+                i++;
+            }
+            while(j>=s && nums[j]>=pivot){
+                j--;
+            }
+            if(i>=j){
+                break;
+            }
+            swap(nums[i],nums[j]);
+        }
+        swap(nums[i],nums[e]);
+        return i;
+    }
+    int findKthLargest(vector<int>& nums, int k) {
+        // priority_queue<int> pq(nums.begin(),nums.end());
+        // int n=k-1;
+        // while(n--){
+        //     pq.pop();
+        // }
+        // return pq.top();
+        int n = nums.size();
+        int s = 0;
+        int e = n-1;
+        int piv = partition(nums,s,e);
+        while(n-piv!=k){
+            int kth = n-piv;
+            if(kth>k){
+                s=piv+1;
+            }else{
+                e=piv-1;
+            }
+            piv = partition(nums,s,e);
+        }
+        return nums[piv];
+
+        // priority_queue<int,vector<int>,greater<int>> pq;
+        // for(int el:nums){
+        //     pq.push(el);
+        //     if(pq.size()>k){
+        //         pq.pop();
+        //     }
+        // }
+        // return pq.top();
+    }
+};
