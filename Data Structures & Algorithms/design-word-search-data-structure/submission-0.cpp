d85ffc51d4334a5efcd973d07fec58ae4e327f44
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-08-05 15:09:05 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.cpp
@@ -0,0 +1,56 @@
+struct Node{
+    char val;
+    bool wordend;
+    unordered_map<char,Node*> children;
+};
+class WordDictionary {
+private:
+Node *root;
+
+public:
+    WordDictionary() {
+        root = new Node;
+        root->val = '\0';
+        root->wordend=false;
+    }
+    
+    void addWord(string word) {
+        Node *curr=root;
+        for(char ch:word){
+            if(!curr->children.contains(ch)){
+            Node *childnode = new Node;
+            childnode->val=ch;
+            childnode->wordend=false;
+            curr->children[ch]=childnode;
+            }
+
+            curr = curr->children[ch];
+        }
+        curr->wordend=true;
+    }
+    
+    bool search(string word,int id=0,Node *start=NULL) {
+        if(!start){
+            start=root;
+        }
+        Node *curr = start;
+        for(int i=id;i<word.size();i++){
+            char ch= word[i];
+            if(ch=='.'){
+                bool ans=false;
+                for(auto it:curr->children){
+                    ans=ans || search(word,i+1,it.second);
+                    if(ans){
+                        return ans;
+                    }
+                }
+                return ans;
+            }
+            if(!curr->children.contains(ch)){
+                return false;
+            }
+            curr=curr->children[ch];
+        }
+        return curr->wordend;
+    }
+};
