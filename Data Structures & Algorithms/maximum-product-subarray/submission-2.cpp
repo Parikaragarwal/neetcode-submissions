732291e4d69fd7e5b26fecf22fea0a09daef5237
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-08-17 13:10:12 +0530

    Add: maximum-product-subarray - submission-2

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-2.cpp
@@ -0,0 +1,43 @@
+class Solution {
+private:
+    int maxprod(vector<int> &nums,int start,int end){
+        int n = end-start+1;
+        int prev=1;
+        int suff=1;
+        int ans = nums[start];
+
+        for(int i=start;i<=end;i++){
+            prev = prev*nums[i];
+            ans = max(ans,prev);
+        }
+        for(int i=end;i>=start;i--){
+            suff = suff*nums[i];
+            ans = max(ans,suff);
+        }
+        return ans;
+    }
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        int start=0;
+        int ans = nums[0];
+        bool zero = false;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){
+                zero=true;
+                start=i+1;
+                continue;
+            }
+            if(i+1==n || nums[i+1]==0){
+                if(i+1<n){
+                    zero=true;
+                }
+                ans = max(ans,maxprod(nums,start,i));
+                start=i+2;
+                i++;
+            }
+        }
+        ans = zero?max(0,ans):ans;
+        return ans;
+    }
+};
