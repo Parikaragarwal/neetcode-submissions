9d9d87401ea0721b97e7f66a178d7fb6e264b32f
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-08-18 20:29:24 +0530

    Add: word-break - submission-8

---
+++ Data Structures & Algorithms/word-break/submission-8.cpp
@@ -0,0 +1,70 @@
+class Solution {
+private:
+    int wc;
+    bool possible(
+        string s,
+        unordered_set<string> &dict,
+        int start,
+        int takechars,
+        vector<vector<int>> &dp
+    ){
+        int n = s.size();
+        if(start==n){
+            return true;
+        }
+
+        if(takechars>wc || start+takechars>n){
+            return false;
+        }
+
+        if(dp[start][takechars]!=-1){
+            return dp[start][takechars];
+        }
+        string subs = s.substr(start,takechars);
+        if(dict.count(subs) && possible(s,dict,start+takechars,1,dp)){
+           return dp[start][takechars]=(int)true;
+        }
+        return dp[start][takechars]=(int)possible(s,dict,start,takechars+1,dp);
+    }
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict;
+        int n = s.size();
+        int mxword=0;
+        for(const string &s:wordDict){
+            dict.insert(s);
+            mxword = max(mxword,(int)s.size());
+        }
+        wc=mxword;
+        // vector<vector<int>> dp(n,vector<int>(wc+1,-1));
+        // return possible(s,dict,0,1,dp);
+        vector<bool> dp(n,false);
+        // string first = s.substr(0,1);
+        // if(dict.count(first)){
+        //     dp[0]=true;
+        // }
+        if(dict.count(s)){
+            return true;
+        }
+        bool flag = true;
+        for(int i=0;i<n;i++){
+            if(dp[i]==true || (i==0 && flag )){
+                int j = dp[i]?i+1:i;
+                int boundary = min(n,j+wc);
+                string can="";
+                while(j<boundary){
+                    can.push_back(s[j]);
+                    if(dict.count(can)){
+                        dp[j]=true;
+                    }
+                    j++;
+                }
+                if(i==0 && flag){
+                    i--;
+                    flag=false;
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
