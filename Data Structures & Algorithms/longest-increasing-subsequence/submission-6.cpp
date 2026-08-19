d0817d53810ff6dabd1348c7f1148bb448492cf2
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-08-19 19:27:06 +0530

    Add: longest-increasing-subsequence - submission-6

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-6.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int longest(
+        vector<int> &nums,
+        int id,
+        int curr
+    ){
+        int n = nums.size();
+        if(id==n){
+            return 0;
+        }
+        int a = INT_MIN;
+        if(nums[id]>curr){
+            a = 1+longest(nums,id+1,nums[id]);
+        }
+        int b = longest(nums,id+1,curr);
+        return max(a,b);
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+
+
+        int n = nums.size();
+        int finalans = 1;
+        vector<int> dp(n,1);
+        vector<int> tail;
+        tail.push_back(INT_MIN);
+
+        for(int i=0;i<n;i++){
+            int curr = nums[i];
+            int ans = 1;
+
+            int start = 0;
+            int end = tail.size()-1;
+            int mid;
+            while(start<=end){
+                mid = start+(end-start)/2;
+                if(tail[mid]<curr){
+                    ans = mid;
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
+            }
+            if(end==tail.size()-1){
+                tail.push_back(curr);
+            }else{
+                tail[end+1]=min(tail[end+1],curr);
+            }
+
+            // for(int j=0;j<i;j++){
+            //     if(nums[j]<nums[i]){
+            //         ans=max(ans,dp[j]);
+            //     }
+            // }
+            dp[i]+=ans;
+            finalans = max(finalans,dp[i]);
+        }
+
+        return tail.size()-1;
+        // return finalans;
+
+
+        //  return longest(nums,0,INT_MIN);
+    }
+};
