874a8e6fc7fe32d53ee02c03b8fe4372b7cef127
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-08-20 14:26:57 +0530

    Add: count-paths - submission-0

---
+++ Data Structures & Algorithms/count-paths/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int uniquePaths(int n, int m) {
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        dp[0][0]=1;
+        int ans = 0;
+        for(int j=0;j<m+n-1;j++){
+            int di=max(0,j-(m-1));
+            int dj=min(j,m-1);
+            while(di<n && dj>=0){
+                int a=0,b=0;
+                if(di-1>=0){
+                    a=dp[di-1][dj];
+                }
+                if(dj-1>=0){
+                    b=dp[di][dj-1];
+                }
+                dp[di][dj]+= a+b;
+                ans = max(ans,a+b);
+                di++;
+                dj--;
+            }
+        }
+        return dp[n-1][m-1];
+    }
+};
