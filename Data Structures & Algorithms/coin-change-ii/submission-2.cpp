186f0fbee858090addaa7cb5a0d90eb4db20472d
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-08-24 17:04:32 +0530

    Add: coin-change-ii - submission-2

---
+++ Data Structures & Algorithms/coin-change-ii/submission-2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int choices(
+        vector<int> &coins,
+        int amount,
+        int id,
+        vector<vector<int>> &dp
+    ){
+        int n = coins.size();
+        if(amount==0){
+            return 1;
+        }
+        if(amount<0 || id>=n){
+            return 0;
+        }
+        if(dp[id][amount]!=-1){
+            return dp[id][amount];
+        }
+        return dp[id][amount]=(choices(coins,amount-coins[id],id,dp)+choices(coins,amount,id+1,dp));
+    }
+    int change(int amount, vector<int>& coins) {
+        if(amount==0){
+            return 1;
+        }
+        int n = coins.size();
+        // vector<vector<int>> dp(n,vector<int>(amount+1,-1));
+        // return choices(coins,amount,0,dp);
+        vector<vector<int>> dp(amount+1,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            dp[0][i]=1;
+        }
+        for(int i=1;i<=amount;i++){
+            for(int j=0;j<n;j++){
+                int take = i-coins[j]<0?0:dp[i-coins[j]][j];
+                int notake = j-1<0?0:dp[i][j-1];
+                dp[i][j]= take+notake;
+            }
+        }
+        return dp[amount][n-1];
+    }
+};
