93392fb0143a39c660490a621022236fa155699c
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-08-27 18:27:08 +0530

    Add: regular-expression-matching - submission-10

---
+++ Data Structures & Algorithms/regular-expression-matching/submission-10.cpp
@@ -0,0 +1,97 @@
+class Solution {
+public:
+    bool match(
+        string &s,
+        string &p,
+        int i,
+        int j,
+        vector<vector<int>> &dp
+    ){
+        int n = s.size();
+        int m = p.size();
+        if(i==n || j>=m){
+            if(j+1<m && p[j+1]=='*'){
+                return match(s,p,i,j+2,dp);
+            }
+            return i==n && j==m;
+        }
+        if(dp[i][j]!=-1){
+            return (bool)dp[i][j];
+        }
+        if(j+1<m && p[j+1]=='*'){
+            bool ans = match(s,p,i,j+2,dp);
+            if(s[i]==p[j] || p[j]=='.'){
+                ans = ans || match(s,p,i+1,j,dp);
+            }
+            return dp[i][j]=(int)ans;
+        }else{
+            if(s[i]==p[j] || p[j]=='.'){
+                return dp[i][j]=(int)match(s,p,i+1,j+1,dp);
+            }else{
+                return dp[i][j]=(int)false;
+            }
+        }
+
+    }
+    bool isMatch(string s, string p) {
+        int n = s.size();
+        int m = p.size();
+        
+        // vector<vector<int>> dp(n,vector<int>(m,-1));
+        // return match(s,p,0,0,dp);
+
+        // vector<vector<bool>> dp(n+1,vector<bool>(m+1,false));
+        // dp[n][m]=true;
+        // for(int j=m-1;j>=0;j--){
+        //     dp[n][j]=j+1<m && p[j+1]=='*' && j+2<=m && dp[n][j+2];
+        // }
+        // for(int i=n-1;i>=0;i--){
+        //     for(int j=m-1;j>=0;j--){
+
+        //     if(j+1<m && p[j+1]=='*'){
+        //         bool ans = dp[i][j+2];
+        //             if(s[i]==p[j] || p[j]=='.'){
+        //              ans = ans || dp[i+1][j];
+        //             }
+        //         dp[i][j]=ans;
+        //     }else{
+        //         if(s[i]==p[j] || p[j]=='.'){
+        //           dp[i][j]=dp[i+1][j+1];
+        //         } else{
+        //           dp[i][j]=false;
+        //         }
+        //     }
+
+        //     }
+        // }
+        // return dp[0][0];
+
+        vector<bool> dp(m+1,false);
+        dp[m]=true;
+        for(int j=m-1;j>=0;j--){
+            dp[j]=j+1<m && p[j+1]=='*' && j+2<=m && dp[j+2];
+        }
+        for(int i=n-1;i>=0;i--){
+            int br = dp[m];
+            dp[m]=false;
+            for(int j=m-1;j>=0;j--){
+            int oldij = dp[j];
+            if(j+1<m && p[j+1]=='*'){
+                bool ans = dp[j+2];
+                    if(s[i]==p[j] || p[j]=='.'){
+                     ans = ans || dp[j];
+                    }
+                dp[j]=ans;
+            }else{
+                if(s[i]==p[j] || p[j]=='.'){
+                  dp[j]=br;
+                } else{
+                  dp[j]=false;
+                }
+            }
+            br = oldij;
+            }
+        }
+        return dp[0];
+    }
+};
