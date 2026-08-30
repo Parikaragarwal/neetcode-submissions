2f5ee8ab50c2d0f9820d763556eb4f8a53290ec3
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-08-31 01:53:45 +0530

    Add: insert-new-interval - submission-13

---
+++ Data Structures & Algorithms/insert-new-interval/submission-13.cpp
@@ -0,0 +1,96 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+      int n = intervals.size();
+      if(n==0){
+        return {newInterval};
+      }
+      vector<vector<int>> ans;
+      int a = newInterval[0];
+      int b = newInterval[1];
+      if(a<intervals[0][0] && b>intervals[n-1][1]){
+        return {newInterval};
+      }
+      if(b<intervals[0][0]){
+        ans.push_back(newInterval);
+        for(auto &inv:intervals){
+            ans.push_back(inv);
+        }
+        return ans;
+      }
+      if(a>intervals[n-1][1]){
+        intervals.push_back(newInterval);
+        return intervals;
+      }
+
+      int start = -1;
+      int end = -1;
+      for(int i=0;i<n;i++){
+        int cs = intervals[i][0];
+        int ce = intervals[i][1];
+        if(a>=cs && a<=ce){
+            start=i;
+        }
+        if(b>=cs && b<=ce){
+            end = i;
+        }
+      }
+      if(start == -1 && end == -1){
+        bool flag = false;
+        for(int i=0;i<n;i++){
+            int cs = intervals[i][0];
+            int ce = intervals[i][1];
+            if(cs>a && !flag){
+                ans.push_back(newInterval);
+                while(i<n && intervals[i][1]<=b)
+                {
+                    i++;
+                }
+                flag=true;
+                if(i<n){
+                    ans.push_back(intervals[i]);
+                }
+            }else{
+            ans.push_back(intervals[i]);
+            }
+        }
+      }else if(start!=-1 && end!=-1){
+        newInterval[0] = intervals[start][0];
+        newInterval[1] = intervals[end][1];
+        for(int i=0;i<n;i++){
+            if(i==start){
+                ans.push_back(newInterval);
+                i=end;
+            }else{
+                ans.push_back(intervals[i]);
+            }
+        }
+      }else{
+        if(start!=-1){
+            for(int i=0;i<n;i++){
+                if(i==start){
+                    intervals[start][1]=b;
+                    ans.push_back(intervals[i]);
+                    while(i+1<n && intervals[i+1][1]<b){
+                        i++;
+                    }
+                }else{
+                    ans.push_back(intervals[i]);
+                }
+            }
+            return ans;
+        }else{
+            intervals[end][0]=a;
+            for(int i=0;i<n;i++){
+               if(intervals[i][0]>=a && i<end){
+                continue;
+               }else{
+                ans.push_back(intervals[i]);
+               }
+            }
+            return ans;
+        }
+      }
+      return ans;
+    }
+};
