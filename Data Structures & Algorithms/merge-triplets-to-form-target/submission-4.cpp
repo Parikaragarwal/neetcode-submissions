bb9fbb7057a569d3a6c0a773fd361a98a6e56910
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-08-30 13:20:18 +0530

    Add: merge-triplets-to-form-target - submission-4

---
+++ Data Structures & Algorithms/merge-triplets-to-form-target/submission-4.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
+        vector<int> ans = {INT_MIN,INT_MIN,INT_MIN};
+        for(auto triplet:triplets){
+            bool skip = false;
+            for(int i=0;i<3;i++){
+                if(triplet[i]>target[i]){
+                    // triplet={INT_MIN,INT_MIN,INT_MIN};
+                    skip=true;
+                    break;
+                }
+            }
+            if(skip){
+                continue;
+            }
+            for(int i=0;i<3;i++){
+                ans[i]=max(ans[i],triplet[i]);
+            }
+            if(ans==target){
+                return true;
+            }
+        }
+        return false;
+    }
+};
