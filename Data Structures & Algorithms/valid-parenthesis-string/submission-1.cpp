af79f2a81299cf540cfe1da29bbcd9046ef82036
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-08-30 22:27:27 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        stack<int> open;
+        stack<int> star;
+        int n = s.size();
+        for(int i=0;i<n;i++){
+            char ch = s[i];
+            if(ch=='('){
+                open.push(i);
+            }else if(ch=='*'){
+                star.push(i);
+            }else{
+                if(!open.empty()){
+                    open.pop();
+                }else if(!star.empty()){
+                    star.pop();
+                }else{
+                    return false;
+                }
+            }
+        }
+        while(!open.empty() && !star.empty()){
+           if(star.top()>open.top()){
+            star.pop();
+            open.pop();
+           }else{
+            return false;
+           }
+        }
+        return open.empty();
+    }
+};
