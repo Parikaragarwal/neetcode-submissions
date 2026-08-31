a571764aa5042e5cfbb640b2fee5ddff271959eb
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-08-31 22:00:21 +0530

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<Interval>& intervals) {
+            sort(intervals.begin(),intervals.end(),[](const auto &a,const auto &b){
+                return a.start<b.start;
+            });
+            int n = intervals.size();
+            if(n==1){
+                return true;
+            }
+            int prevend = INT_MIN;
+            for(int i=0;i<n;i++){
+                if(intervals[i].start<prevend){
+                    return false;
+                }else{
+                    prevend = intervals[i].end;
+                }
+            }
+            return true;
+    }
+};
