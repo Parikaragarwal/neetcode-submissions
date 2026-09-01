1d993fee69ef358002e478f98f339c6482cbfbec
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-09-01 19:53:05 +0530

    Add: spiral-matrix - submission-7

---
+++ Data Structures & Algorithms/spiral-matrix/submission-7.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    void iterate(array<int,2> index,int var,int end,
+    vector<int> &ans,bool inc,
+    vector<vector<int>> &matrix){
+       
+        while(true){
+            if(index[var]==end){
+                ans.push_back(matrix[index[0]][index[1]]);
+                break;
+            }
+            ans.push_back(matrix[index[0]][index[1]]);
+            if(inc){
+                index[var]++;
+            }else{
+                index[var]--;
+            }
+        }
+    }
+    void addlayer(int s,int re,int ce,vector<vector<int>> &matrix,vector<int> &ans){
+        if(s==re || s==ce){
+            if(s==re && s==ce){
+               ans.push_back(matrix[s][re]);
+               return;
+            }
+        if(s!=re){
+        for(int i=s;i<=re;i++){
+            ans.push_back(matrix[s][i]);
+        }
+        }else{
+        for(int i=s;i<=ce;i++){
+            ans.push_back(matrix[i][s]);
+        }
+        }
+        return;
+        }
+        if(s<=re-1) iterate({s,s}  ,1, re-1 ,ans,true,matrix);
+        if(s<=ce-1) iterate({s,re} ,0, ce-1 ,ans,true,matrix);
+        if(re>=s+1) iterate({ce,re},1, s+1  ,ans,false,matrix);
+        if(ce>=s+1) iterate({ce,s} ,0, s+1  ,ans,false,matrix);
+    }
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+       int start=0;
+       int n = matrix.size();
+       int m = matrix[0].size();
+       int colend = n-1;
+       int rowend = m-1;
+       vector<int> ans;
+       while(start<=rowend && start<=colend){
+         addlayer(start,rowend,colend,matrix,ans);
+         start++;
+         rowend--;
+         colend--;
+       }
+       int s = start;
+       int re = rowend;
+       int ce = colend;
+    
+       return ans;
+    }
+};
