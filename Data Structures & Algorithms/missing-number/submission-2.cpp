eafb4b02a6e65a114063d6814a6c2a81afdd1886
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-09-03 15:03:38 +0530

    Add: missing-number - submission-2

---
+++ Data Structures & Algorithms/missing-number/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        // n = n*(n+1)/2;
+
+        int xr=n;
+        for(int i=0;i<n;i++){
+            xr = xr^i;
+            xr = xr^(nums[i]);
+        }
+        return xr;
+        return n;
+    }
+};
