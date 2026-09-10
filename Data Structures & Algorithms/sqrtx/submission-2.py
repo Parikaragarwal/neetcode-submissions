e1874bb469849501c9a8dbd569b142eed89d902c
Parikar Agarwal <160996329+Parikaragarwal@users.noreply.github.com>
2026-09-10 17:44:17 +0530

    Add: sqrtx - submission-2

---
+++ Data Structures & Algorithms/sqrtx/submission-2.py
@@ -0,0 +1,19 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x<2:
+            return x
+
+        start:int = 2
+        end:int = 47343
+
+        while start<=end:
+            mid = start+(end-start)//2
+            if mid*mid == x:
+                return mid
+            elif mid*mid < x:
+                start=mid+1
+            else:
+                end=mid-1
+            
+        return end
+            
